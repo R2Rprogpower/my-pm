e9795c0737efb3584e399ff4430ff452f350bf04
Ruslan Ragimov <ruslan.ragimov@dataart.com>
2019-11-27 19:27:04 +0200

    almost done timeshhets and almost refactored code

--- sites/all/modules/custom/ajax_detach/ajax_detach.js
+++ sites/all/modules/custom/ajax_detach/ajax_detach.js
@@ -5,218 +5,139 @@
  *
  */
 (function($) {
-
-
-
-
-    Drupal.behaviors.ajax_detach = {
-        attach: function(context, settings) {
-            
-          
-                  
-                  
-            $('.custom-class-node-title').find('a').attr('style', 'text-decoration: none; color: black;');
-            $('a:contains("New ticket")').css('color', 'white');
-            $('a:contains("Add a skill")').css('color', 'white');
-            $('a:contains("Dashboard")').css('color', 'white');
-            $('a:contains("Add a team member")').css('color', 'white');
-            $('form-item-end-date').css('color','red');
-            $('h6:contains("Projects")').attr('id', 'custom-h6-project-style');
-            $('h2:contains("Team")').attr('id', 'custom-h6-team-style');
-            $('caption').attr('class', 'custom_caption');
-
-
-            $('.add_to_team_custom.ctools-use-modal-processed').attr('id', 'add_to_team_custom');
-            $('#user-add-to-project-form').css('background-color', 'black');
-
-            var list_open = $('#list-open').find('.item-list');
-
-            var list_development = $('#list-dev').find('.item-list');
-
-            var list_ready = $('#list-red').find('.item-list');
-
-            var list_test = $('#list-test').find('.item-list');
-
-            var list_closed = $('#list-closed').find('.item-list');
-
-            $('#role-label').css('padding-top','10px');
-
-            var drag_area = $('li.custom-row-class ').mousedown(function() {
-                  drag_area.draggable({
-                    containment: ".row",
-                    cursor: "move",
-                    helper: function() {
-                        $(this).removeClass('dropped');
-                        return $(this).addClass('catched');
-                    },
-                    revert: "invalid", // when not dropped, the item will revert back to its initial position
-
-                });
-            });
+  Drupal.behaviors.ajax_detach = {
+    attach: function(context, settings) {
+      $('.custom-class-node-title').find('a').attr('style', 'text-decoration: none; color: black;');
+      $('a:contains("New ticket")').css('color', 'white');
+      $('a:contains("Add a skill")').css('color', 'white');
+      $('a:contains("Dashboard")').css('color', 'white');
+      $('a:contains("Add a team member")').css('color', 'white');
+      $('form-item-end-date').css('color','red');
+      $('h6:contains("Projects")').attr('id', 'custom-h6-project-style');
+      $('h2:contains("Team")').attr('id', 'custom-h6-team-style');
+      $('caption').attr('class', 'custom_caption');
+      $('.add_to_team_custom.ctools-use-modal-processed').attr('id', 'add_to_team_custom');
+      $('#user-add-to-project-form').css('background-color', 'black');
+      $('#role-label').css('padding-top','10px');
       
-            list_open.mouseover(function() {
-                var drop_area = list_open;
-                drop_area.droppable({
-                    accept: drag_area,
-                    drop: function(ev, ui) {
-
-                        var droppedItem = $(ui.draggable);
-                        droppedItem.removeAttr("style");
-                        drop_area.append(droppedItem);
-                        droppedItem.addClass('dropped');
-                        var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
-
-
-                        //var newStatus = drop_area.parent().parent().parent().parent().parent().attr('id');
-                        var newStatus = 2;
-                        var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
-
-
-
-                        if (jQuery.post(myUrl, 0)) {
-                           console.log('Post rq is sent to  '+myUrl);
-
-                        }
-
-
-                    }
-                });
-
-
-            });
-
-            list_ready.mouseover(function() {
-                var drop_area = list_ready;
-                drop_area.droppable({
-                    accept: drag_area,
-                    drop: function(ev, ui) {
-
-                        var droppedItem = $(ui.draggable);
-                        droppedItem.removeAttr("style");
-                        drop_area.append(droppedItem);
-                        droppedItem.addClass('dropped');
-                        var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
-
-                        //  var newStatus = 'Ready for QA';
-                        var newStatus = 4;
-                        var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
-
-                        if (jQuery.post(myUrl, 0)) {
-                            //console.log('Post rq is sent to  '+myUrl);
-
-                        }
-                    }
-                });
-
-
-            });
-            list_development.mouseover(function() {
-                var drop_area = list_development;
-                drop_area.droppable({
-                    accept: drag_area,
-                    drop: function(ev, ui) {
-
-                        var droppedItem = $(ui.draggable);
-                        droppedItem.removeAttr("style");
-                        drop_area.append(droppedItem);
-                        droppedItem.addClass('dropped');
-                        var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
-                        // var newStatus = 'Development';
-                        var newStatus = 3;
-                        var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
-
-                        if (jQuery.post(myUrl, 0)) {
-                            //console.log('Post rq is sent to  '+myUrl);
-
-                        }
-                    }
-                });
-
- 
-            });
-            list_test.mouseover(function() {
-                var drop_area = list_test;
-                drop_area.droppable({
-                    accept: drag_area,
-                    drop: function(ev, ui) {
-
-                        var droppedItem = $(ui.draggable);
-                        droppedItem.removeAttr("style");
-                        drop_area.append(droppedItem);
-                        droppedItem.addClass('dropped');
-                        var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
-
-                        //var newStatus = 'Test';
-                        var newStatus = 5;
-                        var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
-
-                        if (jQuery.post(myUrl, 0)) {
-                            // console.log('Post rq is sent to  '+myUrl);
-
-                        }
-                    }
-                });
-
-
-            });
-            list_closed.mouseover(function() {
-                var drop_area = list_closed;
-                drop_area.droppable({
-                    accept: drag_area,
-                    drop: function(ev, ui) {
-
-                        var droppedItem = $(ui.draggable);
-                        droppedItem.removeAttr("style");
-                        drop_area.append(droppedItem);
-                        droppedItem.addClass('dropped');
-                        var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
-
-                        // var newStatus = 'Closed';  
-                        var newStatus = 6;
-                        var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
-
-                        if (jQuery.post(myUrl, 0)) {
-                            // console.log('Post rq is sent to  '+myUrl);
-
-                        }
-                    }
-                });
-
-                //  list_closed.css('background-color', 'pink');
-
-            });
-          //$('.ajax-detach-user').attr('id','ajax-detach-user');
-	 
-
-            $('.ajax-detach-user').click(function(e) {
-                e.preventDefault();
-                
-                var myUrl = $(this).attr('href');
-
-                if ( jQuery.post(myUrl, 0) ) {
-                   // console.log('Post rq is sent to  '+myUrl);
-                    $(this).parent().parent().hide();
-                }
-
-
-
-            });
-    
-         
-         
-         
-          Object.size = function(obj) {
-                var size = 0,
-                    key;
-                for (key in obj) {
-                    if (obj.hasOwnProperty(key)) size++;
-                }
-                return size;
-            };
-         
-         }
-    };
-
-    
-     
+      var list_open = $('#list-open').find('.item-list');
+      var list_development = $('#list-dev').find('.item-list');
+      var list_ready = $('#list-red').find('.item-list');
+      var list_test = $('#list-test').find('.item-list');
+      var list_closed = $('#list-closed').find('.item-list');
+      
+      var drag_area = $('li.custom-row-class ').mousedown(function() {
+        drag_area.draggable({
+          containment: ".row",
+          cursor: "move",
+          helper: function() {
+            $(this).removeClass('dropped');
+            return $(this).addClass('catched');
+          },
+          revert: "invalid", // when not dropped, the item will revert back to its initial position
+        });
+      });
+      list_open.mouseover(function() {
+        var drop_area = list_open;
+        drop_area.droppable({
+          accept: drag_area,
+          drop: function(ev, ui) {
+            var droppedItem = $(ui.draggable);
+            droppedItem.removeAttr("style");
+            drop_area.append(droppedItem);
+            droppedItem.addClass('dropped');
+            var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
+            var newStatus = 2;
+            var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
+            if (jQuery.post(myUrl, 0)) {
+              console.log('Post rq is sent to  '+myUrl);
+            }
+          }
+        });
+      });
+      list_ready.mouseover(function() {
+        var drop_area = list_ready;
+        drop_area.droppable({
+           accept: drag_area,
+           drop: function(ev, ui) {
+             var droppedItem = $(ui.draggable);
+             droppedItem.removeAttr("style");
+             drop_area.append(droppedItem);
+             droppedItem.addClass('dropped');
+             var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
+             var newStatus = 4;
+             var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
+             if (jQuery.post(myUrl, 0)) {
+             }
+           }
+        });
+      });
+      list_development.mouseover(function() {
+        var drop_area = list_development;
+        drop_area.droppable({
+          accept: drag_area,
+          drop: function(ev, ui) {
+            var droppedItem = $(ui.draggable);
+            droppedItem.removeAttr("style");
+            drop_area.append(droppedItem);
+            droppedItem.addClass('dropped');
+            var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
+            var newStatus = 3;
+            var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
+            if (jQuery.post(myUrl, 0)) {
+            }
+          }
+        });
+      });
+      list_test.mouseover(function() {
+        var drop_area = list_test;
+        drop_area.droppable({
+          accept: drag_area,
+          drop: function(ev, ui) {
+            var droppedItem = $(ui.draggable);
+            droppedItem.removeAttr("style");
+            drop_area.append(droppedItem);
+            droppedItem.addClass('dropped');
+            var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
+            var newStatus = 5;
+            var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
+            if (jQuery.post(myUrl, 0)) {
+            }
+          }
+        });
+      });
+      list_closed.mouseover(function() {
+        var drop_area = list_closed;
+        drop_area.droppable({
+          accept: drag_area,
+          drop: function(ev, ui) {
+            var droppedItem = $(ui.draggable);
+            droppedItem.removeAttr("style");
+            drop_area.append(droppedItem);
+            droppedItem.addClass('dropped');
+            var dropped_item_id = droppedItem.find('div.views-field.views-field-nid')[0].innerText;
+            var newStatus = 6;
+            var myUrl = "/change_ticket_status/ajax/" + dropped_item_id + '/' + newStatus;
+            if (jQuery.post(myUrl, 0)) {
+            }
+          }
+        });  
+      });
+      
+      $('.ajax-detach-user').click(function(e) {
+        e.preventDefault();
+        var myUrl = $(this).attr('href');
+        if ( jQuery.post(myUrl, 0) ) {
+          $(this).parent().parent().hide();
+        }
+      });
+      Object.size = function(obj) {
+        var size = 0, key;
+        for(key in obj) {
+          if (obj.hasOwnProperty(key)) size++;
+        }
+        return size;
+      };
+    }
+  };
 }(jQuery));
--- sites/all/modules/custom/timesheets/timesheets.js
+++ sites/all/modules/custom/timesheets/timesheets.js
@@ -6,140 +6,54 @@
  */
 
 (function ($) {
-
-
-	Drupal.behaviors.timesheets = {
-
-
-		attach: function (context, settings) {
-                   
-            
-            
-            
-
-                       $.fn.style_timesheet = function (data) {
-                           var result = Object.keys(data).map(function(key) {
-                                return [Number(key), data[key]];
-                              });
-                                 console.log('data - ' + Object.values(data) + data);
- 			};
-                                        
-                                        
-                                        
-                                        
-                                        
-                                        
-                             
-                              
-                    
-                          
-                           
-                            
-                            
-                        
-			var path = window.location.pathname;
-			//path = path.substr(1);
-			var pathSplit = path.split('/');
-			//console.log(pathSplit);
-			//    console.log(settings);
-                    
-
-			if (pathSplit[3] == 'timesheets') {
-                          //  $('th.days.mon').css('color', 'red');
-                       $(document).ready(function () {  
-                              var dateTh= { obj: $('th.days.mon') };
-       
-                                
-                                    var justDate = dateTh.obj[0].id;   
-                                    var dateArr = justDate.split('/');
-                               
-                                   var date =  dateArr.join('-'); 
-                                  
-                                
-                             // console.log(date);
-                             var data = {"date1" : date} ;
-
-                                    // console.log(data);
-                          
-				// console.log('success');
-				var url1 = path + '/ajax';
-                                   // console.log(url1);
- 					$.ajax({
-						type: 'POST',
-						url: url1 ,
-                                                data: data,
-						dataType: 'html',
-						success: function (timesheetsInfoJson) {
-						             // echo the recieved data                                          
-							var timesheetsInfo = JSON.parse(timesheetsInfoJson );
-                                                         console.log(timesheetsInfo);
-                                                             // affect the display of the views
-                                                             
-                                                             // fill input fields
-//                                                        
-                                                                 $.each(timesheetsInfo, function(index, data){ 
-                                                                  
-                                                                   var ticketRef =  data['ticket_ref'] ;  
-                                                                    
-                                                                   var dateCreated = data['date_created'].replace('-','\\/').replace('-','\\/');
-                                                                   
-                                                                   var projectNid =  data['project'];
-                                                                 
-                                                                 
-                                                                     
-                                                                    
-                                                                   var hoursSpent = data['time_spent']; 
-                                                                   var inputSelector = '#cell_' + ticketRef + '_' + dateCreated;
-                                                                   var totalSelector = '#cell_total_' + projectNid + '_' + dateCreated;
-                                                                         //console.log( totalSelector );
-
-                                                                   
-
-                                                                   if (  $(inputSelector).length > 0  ) {  
-                                                                 
-                                                                     var inputField =  $(inputSelector).find('input'); 
-                                                                     inputField.css('background-color', 'red');
-                                                                     inputField.val( hoursSpent );
-                                                                     
-                                                                     // hours spent must be created of all ticket's info and only then be applied
-                                                                            // here is the way to select the field and ,amipulate it's items
-                                                                    var currentValue = parseInt($(totalSelector).find('#total_sum').html() );
-                                                                     $(totalSelector).find('#total_sum').html (hoursSpent)
-                                                                    
-                                                                } 
-                                                                   
-                                                                  
-                                                                 });
-                                                                 
-                                                                 
-                                                             // make sum num display correctly 
-                                                        
-                                                        
-                                                        
-                                                        
-                                                        
-                                                        
-						}
-					});
-                                        
-                                        
-                                        
-                                          
-
-            
-                                        
- 
-					
-
-			  });
-
-                      
-                     }
-               
-	} 
-    }
+  Drupal.behaviors.timesheets = {
+    attach: function (context, settings) {
+      var path = window.location.pathname;
+      var pathSplit = path.split('/');
+      if (pathSplit[3] == 'timesheets') {
+        $(document).ready(function () {  
+          var dateTh= { obj: $('th.days.mon') };
+          var monDate = dateTh.obj[0].id;   
+          var dateArr = monDate.split('/');
+          var date =  dateArr.join('-'); 
+          var data = {"date1" : date} ;
+          var total_sums = {};
+          var url1 = path + '/ajax';
+          $.ajax({
+            type: 'POST',
+            url: url1 ,
+            data: data,
+            dataType: 'html',
+            success: function (timesheetsInfoJson) {
+              var timesheetsInfo = JSON.parse(timesheetsInfoJson );
+                 // fill input fields
+              $.each(timesheetsInfo, function(index, data){ 
+                var ticketRef =  data['ticket_ref'] ;  
+                var dateCreated = data['date_created'].replace('-','\\/').replace('-','\\/');
+                var projectNid =  data['project'];
+                var hoursSpent = data['time_spent']; 
+                var inputSelector = '#cell_' + ticketRef + '_' + dateCreated;
+                if (total_sums[dateCreated] === undefined) {
+                  total_sums[dateCreated] = 0;
+                }
+                total_sums[dateCreated] =   parseInt(total_sums[dateCreated], 10) +  parseInt(hoursSpent, 10) ;
+                var totalSelector = '#cell_total_' + projectNid + '_' + dateCreated;
+                if ($(inputSelector).length > 0) {  
+                  var inputField =  $(inputSelector).find('input'); 
+                  inputField.css('background-color', 'red');
+                  inputField.val( hoursSpent );
+                     // hours spent must be created of all ticket's info and only then be applied
+                     // here is the way to select the field and ,amipulate it's items
+                  var currentValue = parseInt($(totalSelector).find('#total_sum').html() );
+                  $(totalSelector).find('#total_sum').html ( total_sums[dateCreated]);
+                }   
+              });
+            }
+          });
+        });
+      }
+    } 
+  }
  // Drupal.attachBehaviors(document, Drupal.settings);
-
-
 }(jQuery));
 
